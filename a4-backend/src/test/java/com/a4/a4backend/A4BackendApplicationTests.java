945622cdf0896e12a252de9b45e44122c7691b85
Davaabayar <busdavaabayar@gmail.com>
2019-12-10 10:48:56 -0600

    Starter

---
+++ a4-backend/src/test/java/com/a4/a4backend/A4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a4.a4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
